833a0c11a32253d296507985dd551e6d7061d6e3
BenVariano <varianob@gmail.com>
2020-08-23 23:07:50 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    $('#slides').superslides({
+        animation: 'fade', 
+        play: 5000,
+        pagination: false
+    });
+
+    var typed = new Typed(".typed", {
+        strings: ["Software Developer.", "Computer Engineer.", "Student."],
+        typeSpeed: 70,
+        loop: true,
+        startDelay:1000,
+        showCursor: false
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            938:{
+                items:4
+            }
+        }
+    });
+
+    
+    
+
+
+    var skillsTopOffset = $(".skillsSection").offset().top;
+
+    $(window).scroll(function(){
+        if(window.pageYOffset > skillsTopOffset - $(window).height()+200){
+            $('.chart').easyPieChart({
+            easing:'easeInOut',
+            barColor: '#282828',
+            trackColor: false,
+            scaleColor: false,
+            lineWidth: 4,     
+            size: 152,
+            onStep: function(from, to, percent){
+                $(this.el).find('.percent').text(Math.round(percent));
+            }
+            });
+        }
+    });
+
+}); 
+
